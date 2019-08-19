a53dedfa9681d6bc264df469d2d7f7f8334af831
Your Name <youremail@domain.com>
2019-08-19 21:01:31 +0530

    Build File

---
+++ frontend/build/precache-manifest.6b467af4d236836a84ed409b78a59ad9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c44b8e01b2b1186320848815bc9705c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a40dea15349c4bc2b9",
+    "url": "/static/css/main.c1ec23a0.chunk.css"
+  },
+  {
+    "revision": "0f9deed3bc0f7813e9df",
+    "url": "/static/js/2.73a04d81.chunk.js"
+  },
+  {
+    "revision": "11a40dea15349c4bc2b9",
+    "url": "/static/js/main.e61a73dd.chunk.js"
+  },
+  {
+    "revision": "9211d8afd60ab1f3041b",
+    "url": "/static/js/runtime~main.d96e090d.js"
+  }
+]);
